24d7f946e30c9a9c83f9f5f60d223eb808dc1619
WrongTurn <1805984583@qq.com>
2023-04-01 16:09:07 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "ZonGPT",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ZonGPT",
     statusBarStyle: "black-translucent",
   },
   themeColor: "#fafafa",
